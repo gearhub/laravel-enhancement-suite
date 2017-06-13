f1ac6b7455b59088f1591013508f4bdf8b8b6353
Vince Ruby <vsruby91@gmail.com>
2017-06-13 14:36:00 -0400

    Add Service provider

---
+++ src/LaravelEnhancementSuiteServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace GearHub\LaravelEnhancementSuite;
+
+use GearHub\LaravelEnhancementSuite\Console\RepositoryMakeCommand;
+use Illuminate\Support\ServiceProvider;
+
+class LaravelEnhancementSuiteServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/config.php' => config_path('les.php')
+        ]);
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/config/config.php', 'les');
+    }
+}
