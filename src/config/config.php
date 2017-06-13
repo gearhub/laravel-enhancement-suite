553d57b766cb91199d918746a4ebc9a20b8554a1
Vince Ruby <vsruby91@gmail.com>
2017-06-13 14:34:21 -0400

    Initial Commit

---
+++ src/config/config.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of the Laravel Enhancement Suite package.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Repository Namespace
+    |--------------------------------------------------------------------------
+    |
+    | Specify the Repository namespace that will be appended to the
+    | application's root namespace.
+    |
+    */
+    'repository_namespace' => 'Repositories',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Transformer Namespace
+    |--------------------------------------------------------------------------
+    |
+    | Specify the Transformer namespace that will be appended to the
+    | application's root namespace.
+    |
+    */
+    'transformer_namespace' => 'Transformers',
+
+];
